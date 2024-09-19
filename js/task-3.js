986a4c068d0af0210a2b39b4a29b34a2710b8c34
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2024-09-19 21:43:13 +0300

    bagFix1

--- js/task-3.js
+++ js/task-3.js
@@ -3,7 +3,9 @@ function getElementWidth(content, padding, border) {
     const paddingWidth = parseFloat(padding);
     const borderWidth = parseFloat(border);
     const boxSize = contentWidth + paddingWidth * 2 + borderWidth * 2;
-    return `${boxSize}px`;
+    return `${boxSize}`;
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
